629a76e3edf5a9110f794261f68753e7c2de38af
Otavio Parraga <otavio.parraga@acad.pucrs.br>
2019-04-08 18:32:19 -0300

    just a trial with some changes

--- src/UpperServerThread.java
+++ src/UpperServerThread.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class UpperServerThread extends Thread {
 
   protected DatagramSocket socket = null;
-  protected static HashMap<Peer, String> clubeDoBolinha;
+  protected static ArrayList<Peer> clubeDoBolinha;
 
   public UpperServerThread() throws IOException {
     this("UpperServerThread");
@@ -20,21 +20,42 @@ public class UpperServerThread extends Thread {
     while (true) {
     	if(socket.getLocalPort() == 4500) { //porta para login
           try {
-            byte[] texto = new byte[256];
+//            byte[] texto = new byte[256];
+//            // recebe datagrama
+//            DatagramPacket pacote = new DatagramPacket(texto, texto.length);
+//            socket.receive(pacote);
+//            // processa o que foi recebido
+//            String recebido = new String(pacote.getData(), 0, pacote.getLength());
+//            recebido = recebido.toUpperCase();
+//            texto = recebido.getBytes();
+//            // envia a resposta de volta ao cliente
+//            InetAddress endereco = pacote.getAddress();
+//            int porta = pacote.getPort();
+//            pacote = new DatagramPacket(texto, texto.length, endereco, porta);
+//            socket.send(pacote);
+//            if ( recebido.equals("FIM") )
+//               break;
+        	  byte[] texto = new byte[256];
             // recebe datagrama
             DatagramPacket pacote = new DatagramPacket(texto, texto.length);
             socket.receive(pacote);
             // processa o que foi recebido
             String recebido = new String(pacote.getData(), 0, pacote.getLength());
-            recebido = recebido.toUpperCase();
-            texto = recebido.getBytes();
+            //System.out.println(recebido.toString());
+            System.out.println(pacote.getAddress().toString());
+            ArrayList<String> archives = new ArrayList<String>(Arrays.asList(recebido.split(" ")));
+            //formato da mensagem ip nome arquivos,separados,por,virgula
+            clubeDoBolinha.add(new Peer(pacote.getAddress().toString(), archives));
+            System.out.println("Peers in the network");
+            for(Peer p: clubeDoBolinha) {
+            	System.out.println("IP: "+p.getIp());
+            	System.out.println("Resources: "+p.getResources().toString()+"\n");
+            }
             // envia a resposta de volta ao cliente
             InetAddress endereco = pacote.getAddress();
             int porta = pacote.getPort();
             pacote = new DatagramPacket(texto, texto.length, endereco, porta);
             socket.send(pacote);
-            if ( recebido.equals("FIM") )
-               break;
           } catch (IOException e) {
             e.printStackTrace();
             break;
