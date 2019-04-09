ed41b043929e8cbdf01bea8ab893e41f07b4670d
17104935 <17104935@facin.portoalegre.pucrsnet.br>
2019-04-09 20:55:51 -0300

    creating query

--- src/UpperClient.java
+++ src/UpperClient.java
@@ -4,27 +4,55 @@ import java.util.*;
 
 public class UpperClient {
 
-  public static void main(String[] args) throws IOException {
-    if (args.length != 2) {
-       System.out.println("Uso: java UpperClient <maquina> <texto>");
-       return;
-    }
-    // cria um socket datagrama
-    DatagramSocket socket = new DatagramSocket();
-    // envia um pacote
-    byte[] texto = new byte[256];
-    texto = args[1].getBytes();
-    InetAddress endereco = InetAddress.getByName(args[0]);
-    DatagramPacket pacote = new DatagramPacket(texto, texto.length, endereco, 4500);
-    socket.send(pacote);
-    // obtem a resposta
-    pacote = new DatagramPacket(texto, texto.length);
-    socket.receive(pacote);
-    // mostra a resposta
-    String resposta = new String(pacote.getData(), 0, pacote.getLength());
-    System.out.println("Texto recebido do servidor: " + resposta);
-    // fecha o socket
-    socket.close();
-  }
+	public static void main(String[] args) throws IOException {
+		Scanner in = new Scanner(System.in);
+		byte[] texto = new byte[256];
+		DatagramSocket socket = new DatagramSocket();
+		String auxiliar;
+		if (args.length != 2) {
+			System.out.println("Uso: java UpperClient <maquina> <texto>");
+			return;
+		}
+		while (true) {
+			auxiliar  = in.nextLine();
+			if (auxiliar.equals("1")) { // realiza o login
+				// cria um socket datagrama
+				// envia um pacote
+				texto = args[1].getBytes();
+				InetAddress endereco = InetAddress.getByName(args[0]);
+				DatagramPacket pacote = new DatagramPacket(texto, texto.length, endereco, 4500);
+				socket.send(pacote);
+				// obtem a resposta
+				pacote = new DatagramPacket(texto, texto.length);
+				socket.receive(pacote);
+				// mostra a resposta
+				String resposta = new String(pacote.getData(), 0, pacote.getLength());
+				System.out.println("Texto recebido do servidor: " + resposta);
+				// fecha o socket
+				socket.close();
+			} else if(auxiliar.equals("2")) { //realiza uma pesquisa por peers com o recurso
+				System.out.println("What resource do you want?");
+				texto = in.nextLine().getBytes();
+				//envia o pacote perguntando o recurso
+				InetAddress endereco = InetAddress.getByName(args[0]);
+				DatagramPacket pacote = new DatagramPacket(texto, texto.length, endereco, 4600);
+				socket.send(pacote);
+				
+				//recebe string com os diferentes peers no que o servidor possui
+				pacote = new DatagramPacket(texto, texto.length);
+				socket.receive(pacote);
+				
+				//printa a resposta
+				String resposta = new String(pacote.getData(), 0, pacote.getLength());
+				System.out.println("Peers que possuem o recurso: " + resposta);
+				
+			} else if(auxiliar.equals("3")) { //
+				System.out.println("Get resource from peer");
+				
+			}else if(auxiliar != null) {
+				System.out.println("Write 1 to login and 2 to query");
+			}
+		}
+	}
 
 }
--- src/UpperServerThread.java
+++ src/UpperServerThread.java
@@ -4,23 +4,23 @@ import java.util.*;
 
 public class UpperServerThread extends Thread {
 
-  protected DatagramSocket socket = null;
-  protected static ArrayList<Peer> clubeDoBolinha;
+	protected DatagramSocket socket = null;
+	protected static ArrayList<Peer> clubeDoBolinha;
 
-  public UpperServerThread() throws IOException {
-    this("UpperServerThread");
-  }
+	public UpperServerThread() throws IOException {
+		this("UpperServerThread");
+	}
 
-  public UpperServerThread(String name) throws IOException {
-    super(name);
-    socket = new DatagramSocket(Integer.parseInt(name));
-    clubeDoBolinha = new ArrayList<Peer>();
-  }
+	public UpperServerThread(String name) throws IOException {
+		super(name);
+		socket = new DatagramSocket(Integer.parseInt(name));
+		clubeDoBolinha = new ArrayList<Peer>();
+	}
 
-  public void run() {
-    while (true) {
-    	if(socket.getLocalPort() == 4500) { //porta para login
-          try {
+	public void run() {
+		while (true) {
+			if (socket.getLocalPort() == 4500) { // porta para login
+				try {
 //            byte[] texto = new byte[256];
 //            // recebe datagrama
 //            DatagramPacket pacote = new DatagramPacket(texto, texto.length);
@@ -36,41 +36,62 @@ public class UpperServerThread extends Thread {
 //            socket.send(pacote);
 //            if ( recebido.equals("FIM") )
 //               break;
-        	  byte[] texto = new byte[256];
-            // recebe datagrama
-            DatagramPacket pacote = new DatagramPacket(texto, texto.length);
-            socket.receive(pacote);
-            // processa o que foi recebido
-            String recebido = new String(pacote.getData(), 0, pacote.getLength());
-            //System.out.println(recebido.toString());
-            //System.out.println(pacote.getAddress().toString());
-            ArrayList<String> archives = new ArrayList<String>(Arrays.asList(recebido.split(",")));
-            //formato da mensagem ip nome arquivos,separados,por,virgula
-            Peer newPeer = new Peer(pacote.getAddress().toString(), archives);
-            clubeDoBolinha.add(newPeer);
-            // envia a resposta de volta ao cliente
-            for(Peer p: clubeDoBolinha) {
-            	System.out.println("Peer: ");
-    	      	System.out.println("IP: "+p.getIp());
-    	      	System.out.println("Resources: "+p.getResources().toString()+"\n------------------------\n");
-    	      }
-            InetAddress endereco = pacote.getAddress();
-            int porta = pacote.getPort();
-            pacote = new DatagramPacket(texto, texto.length, endereco, porta);
-            socket.send(pacote);
-          } catch (IOException e) {
-            e.printStackTrace();
-            break;
-          }
-    	} else if (socket.getLocalPort() == 4600) { //porta para responder as queries
-    		
-    	} else if (socket.getLocalPort() == 4700) { //porta para checar se peer se encontra na rede
-    		
-    	}
-    }
-    socket.close();
-    System.out.println("Servidor encerrado...");
-  }
+					byte[] texto = new byte[256];
+					// recebe datagrama
+					DatagramPacket pacote = new DatagramPacket(texto, texto.length);
+					socket.receive(pacote);
+					// processa o que foi recebido
+					String recebido = new String(pacote.getData(), 0, pacote.getLength());
+					// System.out.println(recebido.toString());
+					// System.out.println(pacote.getAddress().toString());
+					ArrayList<String> archives = new ArrayList<String>(Arrays.asList(recebido.split(",")));
+					// formato da mensagem ip nome arquivos,separados,por,virgula
+					Peer newPeer = new Peer(pacote.getAddress().toString(), archives);
+					clubeDoBolinha.add(newPeer);
+					// envia a resposta de volta ao cliente
+					for (Peer p : clubeDoBolinha) {
+						System.out.println("Peer: ");
+						System.out.println("IP: " + p.getIp());
+						System.out
+								.println("Resources: " + p.getResources().toString() + "\n------------------------\n");
+					}
+					InetAddress endereco = pacote.getAddress();
+					int porta = pacote.getPort();
+					pacote = new DatagramPacket(texto, texto.length, endereco, porta);
+					socket.send(pacote);
+				} catch (IOException e) {
+					e.printStackTrace();
+					break;
+				}
+			} else if (socket.getLocalPort() == 4600) { // porta para responder as queries
+				try {
+					byte[] texto = new byte[256];
+					// recebe datagrama
+					DatagramPacket pacote = new DatagramPacket(texto, texto.length);
+					socket.receive(pacote);
+					//interpreta o que foi recebido
+					String recebido = new String(pacote.getData(), 0, pacote.getLength());
+					ArrayList<String> recursosSocilitados = new ArrayList<String>(Arrays.asList(recebido.split(",")));
+					ArrayList<String> peersWithResources = new ArrayList<String>();
+					for (String recurso : recursosSocilitados) {
+						for (Peer p : clubeDoBolinha) {
+							if(p.getResources().contains(recurso)) {
+								peersWithResources.add(p.getIp());
+							}
+						}
+					}
+					
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+					break;
+				}
+			} else if (socket.getLocalPort() == 4700) { // porta para checar se peer se encontra na rede
 
+			}
+		}
+		socket.close();
+		System.out.println("Servidor encerrado...");
+	}
 
 }
